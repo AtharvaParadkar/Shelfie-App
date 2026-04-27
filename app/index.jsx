6c57e1a585397d9c98440ef9334a09362c3b11e5
AtharvaParadkar <atharvaparadkar91@gmail.com>
2026-04-27 12:44:16 +0530

    feat: simplify home component by removing unused imports and improving code clarity

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,6 @@
-import { StyleSheet, Text, View, Image, Platform, useColorScheme } from 'react-native'
+import { StyleSheet } from 'react-native'
 import { Link } from 'expo-router'
 
-import photo from "../assets/image/logo_dark.png"
-import React from 'react'
-import { Colors } from '../constants/Colors';
 import ThemedView from '../components/ThemedView';
 import ThemedCard from '../components/ThemedCard';
 import ThemedLogo from '../components/ThemedLogo';
@@ -12,25 +9,17 @@ import Spacer from '../components/Spacer';
 
 //? Home component to display the home screen of the app
 const home = () => {
-  const colorScheme = useColorScheme();
-  const theme = Colors[colorScheme] ?? Colors.dark
-
   return (
     <ThemedView style={design.container}>
 
-      <ThemedLogo style={design.picture} />
-
+      <ThemedLogo />
       <Spacer />
 
-      <ThemedText style={StyleSheet.title} title = {true}>The Number 1</ThemedText>
+      <ThemedText style={StyleSheet.title} title={true}>The Number 1</ThemedText>
 
       <ThemedText style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</ThemedText>
 
-      <ThemedCard >
-        <ThemedText>
-          A card!
-        </ThemedText>
-      </ThemedCard>
+      {/* <ThemedCard > <ThemedText> A card! </ThemedText> </ThemedCard> */}
 
       <Link href='/about' style={design.link}>
         <ThemedText>About Page</ThemedText>
@@ -48,20 +37,9 @@ export default home
 
 const design = StyleSheet.create({
   container: {
-    // backgroundColor: "#017390",
     flex: 1,
     alignItems: "center",
     justifyContent: 'center'
-  },
-  t1: {
-    color: "#ffffff",
-    fontWeight: "bold",
-    fontSize: 20
-  },
-  t2: {
-    fontSize: 10,
-    marginBottom: 10,
-    marginBottom: 10
   },
   aCard: {
     backgroundColor: '#04f8bf',
@@ -77,6 +55,10 @@ const design = StyleSheet.create({
   },
   picture: {
     marginVertical: 10
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
   },
   link: {
     marginVertical: 10,
