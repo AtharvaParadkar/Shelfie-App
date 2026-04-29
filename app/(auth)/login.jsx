8bdf7dd9f748474b47a367fe191a2dcf56a5db04
AtharvaParadkar <atharvaparadkar91@gmail.com>
2026-04-29 12:30:40 +0530

    feat: implement login and register buttons with form submission handling

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,27 +1,38 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 
 import ThemedView from '../../components/ThemedView'
 import Spacer from '../../components/Spacer'
 import ThemedText from '../../components/ThemedText'
 import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
+import ThemedButton from '../../components/ThemedButton'
 
 const Login = () => {
-  return (
-    <ThemedView style={styles.container}>
-        <Spacer/>
-        <ThemedText title={true} style={styles.title}>
-            Login to Your Account
-        </ThemedText>
 
-        <Spacer height={100}/>
-        <Link href='/register'>
-            <ThemedText style={{ textAlign: 'center'}}>
-                Register instead
+    const handleSubmit = async () => {
+        console.log('login form submitted')
+    }
+
+    return (
+        <ThemedView style={styles.container}>
+            <Spacer />
+            <ThemedText title={true} style={styles.title}>
+                Login to Your Account
             </ThemedText>
-        </Link>
 
-    </ThemedView>
-  )
+            <ThemedButton onPress={handleSubmit}>
+                <Text style={{ color: '#f2f2f2' }}> Login</Text>
+            </ThemedButton>
+
+            <Spacer height={100} />
+            <Link href='/register'>
+                <ThemedText style={{ textAlign: 'center' }}>
+                    Register instead
+                </ThemedText>
+            </Link>
+
+        </ThemedView>
+    )
 }
 
 export default Login
@@ -29,11 +40,12 @@ export default Login
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center"
+        justifyContent: "center",
+        alignItems: 'center'
     },
     title: {
         textAlign: "center",
-        justifyContent: "center",   
+        justifyContent: "center",
         fontSize: 18,
         marginBottom: 30
     }
--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,27 +1,36 @@
-import { StyleSheet } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text } from 'react-native'
 import ThemedView from '../../components/ThemedView'
 import Spacer from '../../components/Spacer'
 import ThemedText from '../../components/ThemedText'
 import { Link } from 'expo-router'
+import ThemedButton from '../../components/ThemedButton'
 
 const Register = () => {
-  return (
-    <ThemedView style={styles.container}>
-        <Spacer/>
-        <ThemedText title={true} style={styles.title}>
-            Register Your Account
-        </ThemedText>
 
-        <Spacer height={100}/>
-        <Link href='/login'>
-            <ThemedText style={{ textAlign: 'center'}}>
-                Login instead
+    const handleRegister = async () => {
+        console.log('Register form submitted')
+    }
+
+    return (
+        <ThemedView style={styles.container}>
+            <Spacer />
+            <ThemedText title={true} style={styles.title}>
+                Register Your Account
             </ThemedText>
-        </Link>
 
-    </ThemedView>
-  )
+            <ThemedButton onPress={handleRegister} >
+                <Text style={{ color: '#f2f2f2' }}>Register</Text>
+            </ThemedButton>
+
+            <Spacer height={100} />
+            <Link href='/login'>
+                <ThemedText style={{ textAlign: 'center' }}>
+                    Login instead
+                </ThemedText>
+            </Link>
+
+        </ThemedView>
+    )
 }
 
 export default Register
@@ -29,7 +38,8 @@ export default Register
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     title: {
         textAlign: 'center',
